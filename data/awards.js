f772e06a2fd53b67a3f597f31509aca263d3ec02
uom6uoc <uom6uoc@gmail.com>
2020-05-11 21:53:41 +0900

    UPDATED : awards 2020. 5. 11.

--- data/awards.js
+++ data/awards.js
@@ -1,5 +1,27 @@
 var _file_awards = [
   {
+    name: "gwayeok",
+    date: "2020. 5. 11.",
+    who: "과역",
+    title: "달리기",
+    record: "10.76km",
+    time: "45h 23m",
+  },
+  {
+    name: "manggu",
+    date: "2020. 5. 11.",
+    who: "망구",
+    title: "스쿼트",
+    record: "206개",
+  },
+  {
+    name: "ssaekgeun",
+    date: "2020. 5. 11.",
+    who: "쌕근",
+    title: "푸쉬업",
+    record: "50개",
+  },
+  {
     name: "choigeul",
     date: "2020. 5. 10.",
     who: "최글",
@@ -19,14 +41,6 @@ var _file_awards = [
     who: "과역",
     title: "플랭크",
     record: "5m 10s",
-  },
-  {
-    name: "gwayeok",
-    date: "2020. 5. 4.",
-    who: "과역",
-    title: "달리기",
-    record: "9.72km",
-    time: "45h 23m",
   },
   {
     name: "gwayeok",
