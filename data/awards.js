5f039cae385e7a6240afd83078da4d96d61c27c3
uom6uoc <47206086+uom6uoc@users.noreply.github.com>
2020-06-02 09:10:45 +0900

    Update awards.js
    초기화

--- data/awards.js
+++ data/awards.js
@@ -1,37 +1,9 @@
 var _file_awards = [
   {
-    name: "gwayeok",
-    date: "2020. 5. 20.",
-    who: "과역",
-    title: "기마자세",
-    record: "2m 30s",
-  },
-  {
-    name: "ssaekgeun",
-    date: "2020. 5. 17.",
-    who: "쌕근",
-    title: "푸쉬업",
-    record: "63개",
-  },
-  {
-    name: "choigeul",
-    date: "2020. 5. 16.",
-    who: "최글",
-    title: "턱걸이",
-    record: "20개",
-  },
-  {
-    name: "gwayeok",
-    date: "2020. 5. 15.",
-    who: "과역",
-    title: "스쿼트",
-    record: "276개",
-  },
-  {
-    name: "gwayeok",
-    date: "2020. 5. 5.",
-    who: "과역",
-    title: "플랭크",
-    record: "5m 10s",
-  },
+    name: "none",
+    date: "2020. 6. 1.",
+    who: "초기화",
+    title: "6월시작",
+    record: "",
+  }
 ];
