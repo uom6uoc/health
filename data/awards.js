dd5dca1e6cdacccead104b311d0ce512bd19e694
uom6uoc <47206086+uom6uoc@users.noreply.github.com>
2020-06-03 00:51:51 +0900

    Update awards.js
    2020. 6. 2. awards.

--- data/awards.js
+++ data/awards.js
@@ -1,10 +1,31 @@
 var _file_awards = [
   {
     name: "none",
-    date: "2020. 6. 1.",
+    date: "2020. 6. 2.",
     who: "최글",
     title: "턱걸이",
-    record: "10개",
+    record: "20개",
+  },
+  {
+    name: "none",
+    date: "2020. 6. 2.",
+    who: "과역",
+    title: "행잉레그레이즈",
+    record: "26개",
+  },
+  {
+    name: "none",
+    date: "2020. 6. 2.",
+    who: "과역",
+    title: "AB 롤업",
+    record: "45개",
+  },
+  {
+    name: "none",
+    date: "2020. 6. 2.",
+    who: "과역",
+    title: "푸쉬업",
+    record: "50개",
   },
   {
     name: "none",
@@ -16,22 +37,8 @@ var _file_awards = [
   {
     name: "none",
     date: "2020. 6. 1.",
-    who: "망구",
-    title: "푸쉬업",
-    record: "11개",
-  },
-  {
-    name: "none",
-    date: "2020. 6. 1.",
     who: "과역",
     title: "5km달리기",
     record: "5'37",
-  },
-  {
-    name: "none",
-    date: "2020. 6. 1.",
-    who: "홀띠",
-    title: "AB 롤업",
-    record: "32개",
   }
 ];
