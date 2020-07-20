6921b745d1bef29e1dab1500d462d4f2b0f5153f
uom6uoc <47206086+uom6uoc@users.noreply.github.com>
2020-07-21 08:18:07 +0900

    Update records.js

--- data/records.js
+++ data/records.js
@@ -12,6 +12,14 @@ var example = {
 var _file_records = {
   2020: {
     7: {
+  20: {
+    choigeul: { active: true, exercise: {} },
+    gwayeok: { active: true, exercise: {} },
+    manggu: { active: true, exercise: {} },
+    ssaekgeun: { active: true, exercise: {} },
+    ssapjong: { active: true, exercise: {} },
+    holtti: { active: true, exercise: {} },
+  },
   19: {
     choigeul: { active: true, exercise: {} },
     gwayeok: { active: true, exercise: {} },
